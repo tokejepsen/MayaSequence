483bf127f6d3263d943817308b3f2a902961f667
Toke Jepsen <tokejepsen@gmail.com>
2021-05-21 08:25:18 +0100

    Update userSetup.py

--- userSetup.py
+++ userSetup.py
@@ -1,21 +1,28 @@
 import socket
+import os
 
 from maya import cmds
 
 
 def open_deadline_port():
 
-    cmds.commandPort(name=":7005", sourceType="python")
+    cmds.commandPort(
+        name=":{}".format(os.environ["MAYASEQUENCE_RENDER_PORT"]),
+        sourceType="python"
+    )
     print("Deadline port is open.")
 
     # Reply to Deadline server waiting on Maya boot.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("localhost", 10000)
+    server_address = ("localhost", int(os.environ["MAYASEQUENCE_LAUNCH_PORT"]))
     sock.connect(server_address)
 
 
 def load_plugins():
-    cmds.loadPlugin("mtoa.mll", quiet=True)
+    print("Loading plugins...")
+    cmds.loadPlugin("mtoa.mll")
+    cmds.loadPlugin("AbcExport.mll", quiet=True)
+    cmds.loadPlugin("AbcImport.mll", quiet=True)
 
 
 cmds.evalDeferred(load_plugins)
